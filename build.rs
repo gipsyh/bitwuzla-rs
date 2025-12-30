9bac6e37210c2f9c34d1d826c51976d7f639bc90
Yuheng Su <gipsyh.icu@gmail.com>
2025-12-30 17:04:42 +0800

    feat: add vendor feature support in build script

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
 extern crate giputils;
+#[cfg(feature = "vendor")]
 use giputils::build::copy_build;
 use std::{io, process::Command};
 
-fn main() -> io::Result<()> {
+#[cfg(feature = "vendor")]
+fn build_vendor() -> io::Result<()> {
     giputils::build::git_submodule_update()?;
     println!("cargo:rerun-if-changed=./bitwuzla");
     let cb_path = copy_build("bitwuzla", |src| {
@@ -48,5 +50,42 @@ fn main() -> io::Result<()> {
     println!("cargo:rustc-link-lib=dylib=c++");
     println!("cargo:rustc-link-lib=dylib=gmp");
     println!("cargo:rustc-link-lib=dylib=mpfr");
+    Ok(())
+}
+
+#[cfg(not(feature = "vendor"))]
+fn link_system() -> io::Result<()> {
+    if let Ok(output) = Command::new("pkg-config")
+        .args(["--libs", "--cflags", "bitwuzla"])
+        .output()
+        && output.status.success()
+    {
+        let flags = String::from_utf8_lossy(&output.stdout);
+        for flag in flags.split_whitespace() {
+            if let Some(lib) = flag.strip_prefix("-l") {
+                println!("cargo:rustc-link-lib={}", lib);
+            } else if let Some(path) = flag.strip_prefix("-L") {
+                println!("cargo:rustc-link-search=native={}", path);
+            }
+        }
+        return Ok(());
+    }
+    println!("cargo:rustc-link-lib=dylib=bitwuzla");
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-lib=dylib=c++");
+    println!("cargo:rustc-link-lib=dylib=gmp");
+    println!("cargo:rustc-link-lib=dylib=mpfr");
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    #[cfg(feature = "vendor")]
+    build_vendor()?;
+
+    #[cfg(not(feature = "vendor"))]
+    link_system()?;
+
     Ok(())
 }
